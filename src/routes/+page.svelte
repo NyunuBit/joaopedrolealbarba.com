530e1f273f11887f0434569ef0464919b55b2a16
NyunuBit <nyunu@proton.me>
2026-05-14 10:57:34 -0300

    added cute photos of joao

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,23 @@
 <script lang="ts">
 	import Video from "$lib/assets/xwrzm.mp4";
+	import Img from "$lib/assets/7x846.png";
 </script>
 
 <div class="w-full flex-col gap-2 h-screen flex justify-center items-center">
-	<p class="text-2xl italic text-white/50">joão pedro leal barba.</p>
-	<video src={Video} loop autoplay></video>
+	<p class="text-2xl italic text-white/50">
+		joão pedro leal marba (eu escrevi errado).
+	</p>
+	<div class="flex justify-between gap-5">
+		<div
+			style="background-image: url('{Img}');"
+			class="aspect-square bg-contain h-full bg-no-repeat"
+		>
+			<p class="text-shadow-2xs text-4xl p-5 font-black">
+				Esse é joão pedro leal marba
+			</p>
+		</div>
+		<video src={Video} loop autoplay></video>
+	</div>
 </div>
 <div class="absolute bottom-0 w-full p-2 flex justify-between items-center">
 	<a
