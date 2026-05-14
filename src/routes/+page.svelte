d3ff5061f04886a5450022ad4248697ec4bbddc1
NyunuBit <nyunu@proton.me>
2026-05-14 11:06:17 -0300

    responsive designg

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,23 +3,25 @@
 	import Img from "$lib/assets/7x846.png";
 </script>
 
-<div class="w-full flex-col gap-2 h-screen flex justify-center items-center">
+<div class="w-full flex-col gap-2 flex p-5 justify-center items-center">
 	<p class="text-2xl italic text-white/50">
 		joão pedro leal marba (eu escrevi errado).
 	</p>
-	<div class="flex justify-between gap-5">
+	<div
+		class="flex md:justify-between justify-center items-center flex-col md:flex-row gap-5"
+	>
 		<div
 			style="background-image: url('{Img}');"
-			class="aspect-square bg-contain h-full bg-no-repeat"
+			class="aspect-square bg-contain md:w-auto w-screen p-5 bg-no-repeat"
 		>
-			<p class="text-shadow-2xs text-4xl p-5 font-black">
+			<p class="text-shadow-2xs max-w-152 text-4xl w-screen font-black">
 				Esse é joão pedro leal marba
 			</p>
 		</div>
 		<video src={Video} loop autoplay></video>
 	</div>
 </div>
-<div class="absolute bottom-0 w-full p-2 flex justify-between items-center">
+<div class="bottom-0 w-full p-2 flex justify-between items-center">
 	<a
 		href="https://github.com/NyunuBit/joaopedrolealbarba.com"
 		class="underline text-purple-500 hover:text-purple-300 font-bold duration-200"
